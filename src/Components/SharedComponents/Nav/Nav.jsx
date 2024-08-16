406be96467a3da381aba35d9fc43dec52d47379e
Farhad2590 <farhadhossen2590@gmail.com>
2024-08-17 01:12:23 +0600

    Nav bar Updated

--- src/Components/SharedComponents/Nav/Nav.jsx
+++ src/Components/SharedComponents/Nav/Nav.jsx
@@ -1,41 +1,52 @@
 // import { NavLink } from "react-router-dom";
 import { Link } from 'react-router-dom';
 import logo from '../../../assets/logo.png'
+import useAuth from '../../../hooks/useAuth';
 
 const Nav = () => {
+    const { user, logOut } = useAuth()
+
     return (
         <>
-            <div className="flex items-center md:gap-5 justify-between pt-5 ">
+            <div className="flex items-center md:gap-5 justify-between pt-5 pb-5 ">
                 <div className="">
                     <div className="flex items-center">
                         <img className="w-16" src={logo} alt="" />
                         <h2 className="text-green-700 font-bold font-gramond text-xl md:text-xl lg:text-3xl">BanglaBookVault</h2>
                     </div>
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </div>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            {/* <NavLink to={'/'} className={({ isActive }) => isActive ? 'text-green-500 font-bold btn btn-outline' : 'btn btn-outline-none bg-white'}>Home</NavLink> */}
-                            {/* <NavLink to={'/listedBooks'} className={({ isActive }) => isActive ? 'text-green-500 font-bold btn btn-outline' : 'btn btn-outline-none bg-white'}>Listed Books</NavLink>
-                            <NavLink to={'/pagestoread'} className={({ isActive }) => isActive ? 'text-green-500 font-bold btn btn-outline' : 'btn btn-outline-none bg-white'}>Pages To read</NavLink>
-                            <NavLink to={'/bestSeller'} className={({ isActive }) => isActive ? 'text-green-500 font-bold btn btn-outline' : 'btn btn-outline-none bg-white'}>Best Seller</NavLink>
-                            <NavLink to={'/deliveryInfo'} className={({ isActive }) => isActive ? 'text-green-500 font-bold btn btn-outline' : 'btn btn-outline-none bg-white'}>Delivery Info</NavLink> */}
-                        </ul>
-                    </div>
 
                 </div>
-                <div className=" hidden lg:flex ">
-                    <ul className="menu menu-horizontal gap-2">
-                        {/* <NavLink to={'/'} className={({ isActive }) => isActive ? 'text-green-500 font-bold btn btn-outline' : 'btn btn-outline-none bg-white'}>Home</NavLink> */}
-                        {/* <NavLink to={'/listedBooks'} className={({ isActive }) => isActive ? 'text-green-500 font-bold btn btn-outline' : 'btn btn-outline-none bg-white'}>Listed Books</NavLink>
-                        <NavLink to={'/pagestoread'} className={({ isActive }) => isActive ? 'text-green-500 font-bold btn btn-outline' : 'btn btn-outline-none bg-white'}>Pages To read</NavLink>
-                        <NavLink to={'/bestSeller'} className={({ isActive }) => isActive ? 'text-green-500 font-bold btn btn-outline' : 'btn btn-outline-none bg-white'}>Best Seller</NavLink>
-                        <NavLink to={'/deliveryInfo'} className={({ isActive }) => isActive ? 'text-green-500 font-bold btn btn-outline' : 'btn btn-outline-none bg-white'}>Delivery Info</NavLink> */}
-                    </ul>
-                </div>
                 <div className="flex gap-2">
-                    <Link to="/signin" className="btn bg-green-700 text-white">Sign In</Link>
+                    {/* <Link to="/signin" className="btn bg-green-700 text-white">Sign In</Link> */}
+                    {
+                        user ?
+                            <div className="dropdown dropdown-end">
+                                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                    <div className="w-10 rounded-full">
+                                        <img alt="Tailwind CSS Navbar component" src={user && user.photoURL} />
+                                    </div>
+                                </div>
+                                <div tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-50 p-2 shadow bg-base-100 rounded-box w-52">
+                                    <li className="mt-3 btn bg-green-700 text-white">
+                                        <a className="justify-between">
+                                            {
+                                                user && user.displayName
+                                            }
+                                        </a>
+                                    </li>
+                                    <li className="btn bg-green-700 text-white" ><button onClick={logOut}>Logout</button></li>
+                                </div>
+                            </div>
+
+                            :
+                            <Link to="/signin">
+                                <button className="btn bg-green-700  text-white hover:border border-green-700">
+                                    Join Us
+                                </button>
+
+                            </Link>
+
+                    }
                 </div>
             </div>
 
